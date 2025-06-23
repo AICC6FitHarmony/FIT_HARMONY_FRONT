3a3612fc614a00039bc2aa9e7ba42fc26f767f12
91 <thgydnjs@gmail.com>
2025-06-24 01:47:31 +0900

    33

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -4,33 +4,49 @@ import { CalendarDays } from 'lucide-react';
 
 const NavBar = () => {
   return (
-    <div className=" box-border shadow-2xl shadow-green-500">
-      <div className="a">
-        <div className="nav_wrap">
-          <ul>
-            <li>
-              <Link to="/dashboard">1</Link>
+    // logo
+
+    <div className='container'>
+      <div className='flex justify-center items-center h-20'>
+        <span className='text-2xl font-bold'>Fit Harmony</span>
+        </div>
+    
+    <div className="  bg-green-50 sticky top-0 z-50 px-10 shadow-lg shadow-green-800/10">
+      <div className=" ">
+        <div className="flex justify-between items-center h-20 ">
+        
+          <ul className='flex justify-between items-center w-full'>
+            <li className='text-green-700 hover:bg-green-200 rounded-full px-4 py-4 transition font-medium'>
+              <Link to="/dashboard">대쉬보드</Link>
             </li>
-            <li>
+            <li className='text-green-700 hover:bg-green-200 px-4 py-4 rounded-full transition font-medium'>
               <Link to="/schedule">
                 <CalendarDays />
                 캘린더
               </Link>
-            </li>
-            <li>
+            </li >
+            <li className='text-green-700 hover:bg-green-200 rounded-full px-4 py-4 transition font-medium'>
               <Link to="/inbody">인바디</Link>
             </li>
-            <li>
+            <li className='text-green-700 hover:bg-green-200 rounded-full px-4 py-4 transition font-medium'>
               <Link to="/community">커뮤니티</Link>
             </li>
-            <li>
+            <li className='text-green-700 hover:bg-green-200 rounded-full px-4 py-4 transition font-medium'>
               <Link to="/trainer">강사 찾기</Link>
             </li>
           </ul>
         </div>
-      </div>
+        </div>
+        </div>
     </div>
   );
 };
 
 export default NavBar;
+
+
+
+
+
+
+
