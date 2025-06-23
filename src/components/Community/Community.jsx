f556755b8859c5a88a84bd61bc151f171c6c40e9
Wasa-B <ghd1876@gmail.com>
2025-06-23 16:04:28 +0900

    commit

---
+++ src/components/Community/Community.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Community = () => {
+  return (
+    <div className=''>
+      123123
+    </div>
+  )
+}
+
+export default Community
---
+++ src/components/common/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+      <div className="a">
+            <div className="nav_wrap">
+                <ul>
+                    <li>11111</li>
+                    <li>22222</li>
+                    <li>3333</li>
+                    <li>4444</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
