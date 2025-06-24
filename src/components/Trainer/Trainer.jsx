d56c39679edfa34c71df516b8da380372aeadd0c
hyo91 <thgydnjs@gmail.com>
2025-06-24 19:37:04 +0900

    hw

--- src/components/Trainer/Trainer.jsx
+++ src/components/Trainer/Trainer.jsx
@@ -17,57 +17,65 @@ const Trainer = () => {
   };
 
   return (
-    <div className="trainer-container pt-20 justify-center w-full h-full">
-      <div className="search-title">
-        <div className="flex justify-center">
-          <h2 className="text-2xl">강사 찾기</h2>
-        </div>
+    <div className="content-wrapper pt-20 justify-center w-full">
+      <div className="">
+        <div className="search-title">
+          <div className="flex justify-center">
+            <h2 className="text-2xl">강사 찾기</h2>
+          </div>
 
-        <div className="search-title pt-5 flex justify-center items-center">
-          <div className="relative">
-            <input
-              type="text"
-              placeholder="검색어를 입력해 주세요"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-              className="border-2 border-[#1a7d45] rounded-md w-[35rem] h-[40px] shadow-md "
-              style={{ paddingRight: '40px' }}
-            />
-            <button
-              onClick={handleSearch}
-              className="absolute right-0 top-0 mt-2 mr-2 p-2 hover:bg-gray-100 rounded-md"
-              style={{ height: 'calc(100% - 8px)' }}
-            >
-              <IoSearchOutline />
-            </button>
+          <div className="search-title pt-5 flex justify-center items-center">
+            <div className="relative">
+              <input
+                type="text"
+                placeholder="검색어를 입력해 주세요"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                className="border-2 border-[#1a7d45] rounded-md  w-[35rem] h-[40px] shadow-md "
+                style={{ paddingRight: '40px' }}
+              />
+              <button
+                onClick={handleSearch}
+                className="abso lute right-0 hover:text-[#000] text-2xl text-[#1a7d45]"
+                style={{ height: 'calc(100% - 8px)' }}
+              >
+                <IoSearchOutline />
+              </button>
+            </div>
           </div>
         </div>
-      </div>
 
-      {/* 검색 결과 표시 */}
-      <div className="search-results flex justify-center">
-        <div className="trainer-container mt-30 border-2 border-[#1e231e] w-[50%] h-[50%]">
-          <div className="trainer-list">
-            {searchResult.length > 0 ? (
-              searchResult.map((trainer) => (
-                <div key={trainer.id}>
-                  <p>프필사진{trainer.image}</p>
-                  <h3>이름{trainer.name}</h3>
-                  <p>상품설명{trainer.description}</p>
-                  <p>가격{trainer.price}</p>
-
-                  <p>{trainer.date}</p>
-                </div>
-              ))
-            ) : (
-              <p className="justify-center p-15 flex">검색 결과가 없습니다 </p>
-            )}
+        {/* 검색 결과 표시 */}
+        <div className="search-results flex h-full mt-30 relative justify-between">
+          <div className="trainer-container  border-2 border-[#1e231e] w-[70%] h-[40rem] right-0 absolute">
+            <div className="trainer-list w-full h-full align-center justify-center">
+              {searchResult.length > 0 ? (
+                searchResult.map((trainer) => (
+                  <div key={trainer.id}>
+                    <p>프필사진{trainer.image}</p>
+                    <h3>이름{trainer.name}</h3>
+                    <p>상품설명{trainer.description}</p>
+                    <p>가격{trainer.price}</p>
+                    <p>{trainer.date}</p>
+                  </div>
+                ))
+              ) : (
+                <p className="justify-center h-full  p-15 flex">
+                  검색 결과가 없습니다{' '}
+                </p>
+              )}
+            </div>
+          </div>
+          <div className="nav w-full h-full">
+            <div className="trainer-filter border-1 absolute h-full w-[20%]">
+              border
+            </div>
           </div>
         </div>
+
+        <div className="check"></div>
+        <div className="under">h</div>
       </div>
-      <div className="nav"></div>
-      <div className="check"></div>
-      <div className="under"></div>
     </div>
   );
 };
