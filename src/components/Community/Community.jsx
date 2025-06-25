1142f5726be05be9aef8cc1297ebf26c4339769b
Wasa-B <ghd1876@gmail.com>
2025-06-25 17:45:51 +0900

    레이아웃 구성

--- src/components/Community/Community.jsx
+++ src/components/Community/Community.jsx
@@ -3,13 +3,40 @@ import { AuthProvider, useAuthRedirect } from '../../js/login/AuthContext'
 import StandardModal from '../cmmn/StandardModal';
 
 const Community = () => {
-  const { user, loading } = useAuthRedirect();
-  console.log("User : ",user);
-  console.log(loading);
-  return (
-      <div className=''>
 
-        123123
+  const totalCount = 0;
+
+  // const { user, loading } = useAuthRedirect();
+  // console.log("User : ",user);
+  // console.log(loading);
+  return (
+      <div className='board_wrapper border'>
+        <div className='board_header border'>
+          <div className='board_title'>커뮤니티</div>
+          <div className="board_action flex justify-between">
+            <div className="search border">
+              <span>검색</span>
+              <input type="text" />
+              <select name="" id=""> 
+                <option value="title">제목</option>
+                <option value="content">내용</option>
+                <option value="user">작성자</option>
+              </select>
+            </div>
+            <div className="info_and_create">
+              <span>total : {totalCount}</span>
+              <button>생성</button>
+            </div>
+          </div>
+        </div>
+        <div className="board_body">
+          
+        </div>
+        <div className="board_footer">
+          <div className="boar_nav">
+
+          </div>
+        </div>
       </div>
   )
 }
---
+++ src/components/Community/CommunityBoard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CommunityBoard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CommunityBoard
