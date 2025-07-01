c39b49f4e77e15eede97d91cfca64fc6fd9fdbda
Wasa-B <ghd1876@gmail.com>
2025-07-01 18:23:27 +0900

    input-update

--- src/components/cmmn/InputWithLabel.jsx
+++ src/components/cmmn/InputWithLabel.jsx
@@ -9,7 +9,7 @@ const InputWithLabel = ({label, name, value, onChange, isNumber, waringText, isW
   }
 
   return (
-    <div className="input-label w-full relative flex gap-2 items-center justify-between">
+    <div className="input-with-label w-full relative flex gap-2 items-center justify-between">
       {label&&(<div className='label'>{label}</div>)}
       <div className={`input-wrapper flex flex-col ${label?"w-[80%]":"w-full"}`}>
         <input
@@ -20,7 +20,7 @@ const InputWithLabel = ({label, name, value, onChange, isNumber, waringText, isW
           type="text"
           />
         {isWaring ? (
-              <p className="text-sm text-red-500 absolute bottom-[-17px]">
+              <p className="waring-text text-sm text-red-500 absolute bottom-[-17px]">
                 {waringText}
               </p>
             ) : (
