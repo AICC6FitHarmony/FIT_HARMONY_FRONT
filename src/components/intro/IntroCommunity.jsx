070b048fe74052ab3e30becad5fc276ae668b427
김준영 <1090jun@naver.com>
2025-07-09 12:03:15 +0900

    인바디OCR+OPENAI_마이페이지updateQuery_인트로Link

--- src/components/intro/IntroCommunity.jsx
+++ src/components/intro/IntroCommunity.jsx
@@ -1,26 +1,31 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-const IntroCommunity = ({getHotData, getLatestData}) => {
-  const [hotData , setHotData] = useState();
-  const [latestData , setLatestData] = useState();
-  useEffect(()=>{
+const IntroCommunity = ({ getHotData, getLatestData }) => {
+  const [hotData, setHotData] = useState();
+  const [latestData, setLatestData] = useState();
+  useEffect(() => {
     setHotData(getHotData);
     setLatestData(getLatestData);
-  },[getHotData,getLatestData])
+  }, [getHotData, getLatestData]);
 
   return (
     <div className="mt-10">
       <div className="grid grid-cols-2 gap-4">
         <div className="space-y-2">
           <h2 className="text-lg font-semibold mb-2">인기 게시글</h2>
-          {hotData && hotData.map((item, idx) => (
+          {hotData &&
+            hotData.map((item, idx) => (
               <div key={idx} className="flex items-center justify-between">
-                <a href={`/community/post/${item.postId}`} className="flex-1 flex items-center">
+                <a
+                  href={`/community/post/${item.postId}`}
+                  className="flex-1 flex items-center"
+                >
                   <span>{item.title}</span>
                   <span className="ml-2 text-gray-400 text-sm">{`[${item.commentCount}]`}</span>
                 </a>
               </div>
-          ))}
+            ))}
           <button className="text-sm text-[#8e8e8e]">
             <a href="/community">더보기</a>
           </button>
@@ -28,14 +33,18 @@ const IntroCommunity = ({getHotData, getLatestData}) => {
 
         <div className="space-y-2">
           <h2 className="text-lg font-semibold mb-2">최근 게시글</h2>
-          {latestData && latestData.map((item, idx) => (
+          {latestData &&
+            latestData.map((item, idx) => (
               <div key={idx} className="flex items-center justify-between">
-                <a href={`/community/post/${item.postId}`} className="flex-1 flex items-center">
+                <Link
+                  to={`/community/post/${item.postId}`}
+                  className="flex-1 flex items-center"
+                >
                   <span>{item.title}</span>
                   <span className="ml-2 text-gray-400 text-sm">{`[${item.commentCount}]`}</span>
-                </a>
+                </Link>
               </div>
-          ))}
+            ))}
           <button className="text-sm text-[#8e8e8e]">
             <a href="/community">더보기</a>
           </button>
